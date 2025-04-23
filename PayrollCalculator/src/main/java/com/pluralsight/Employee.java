5780bddbf6994df2222b1d68201fc79ce68fcfc0
James Bosley <jamesbosley123@gmail.com>
2025-04-23 16:23:19 -0500

    Completed workbook 3 exercises

---
+++ PayrollCalculator/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+
+public class Employee {
+    private String employeeId;
+    private String name;
+    private float hoursWorked;
+    private float payRate;
+
+    public Employee(String employeeId, String name, float hoursWorked, float payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public float getGrossPay() {
+        return this.hoursWorked * this.payRate;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(float hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public float getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(float payRate) {
+        this.payRate = payRate;
+    }
+}
--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,9 +1,58 @@
 package com.pluralsight;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
+        try {
+            // file to pull from and write to
+            System.out.println("Enter the name of the file employee file to process:");
+            String fileName = scan.nextLine();
+            System.out.println("Enter the name of the payroll file to create:");
+            String newFile = scan.nextLine();
+
+            FileReader fileReader = new FileReader("src/main/resources/" + fileName);
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            FileWriter writer = new FileWriter("src/main/resources/" + newFile);
+            BufferedWriter bufWriter = new BufferedWriter(writer);
+
+
+            String input;
+            bufWriter.write("Id|Name|Gross pay \n");
+            while((input = bufReader.readLine()) != null) {
+                String[] employeeParts = input.split("\\|");
+
+               if(employeeParts[0].equals("id")) {
+                    continue;
+                }
+                Employee newEmployee = new Employee(employeeParts[0], employeeParts[1],
+                        Float.parseFloat(employeeParts[2]), Float.parseFloat(employeeParts[3]) );
+
+                System.out.printf("Employee Id: %s \n Employee Name: %s \n Gross pay is: %f",
+                        newEmployee.getEmployeeId(), newEmployee.getName(), newEmployee.getGrossPay());
+
+                String lineforNewFile = newEmployee.getEmployeeId() + ", " + newEmployee.getName() +
+                        ", " + newEmployee.getGrossPay() + "\n";
+
+
+                bufWriter.write(lineforNewFile);
+
+            }
+            bufWriter.close();
+            bufReader.close();
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+        //Old code----------------------------------
+       /* Scanner scan = new Scanner(System.in);
 
         System.out.println("What is your name");
         String name = scan.nextLine();
@@ -17,7 +66,7 @@ public class PayrollCalculator {
         float result = hoursWorked * payRate;
 
         System.out.printf("The good man %s made %.2f last week.", name, result);
-
+        */
 
     }
 }
